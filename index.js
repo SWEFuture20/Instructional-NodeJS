160bea253fdba2648adcc42018398838635ba0ff
Latifah Lillie <queen1892@gmail.com>
2021-02-28 18:33:11 -0600

    Adding node modules, gitignore, index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// array of questions for user
+const questions = [ 
+    "How old are your?", 
+    "Do you prefer a truck or car?",
+    "Are you and early bird or night owl?" ];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
