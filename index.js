c79aefa787606222aa68917cea91cd44b7bf5e35
Latifah Lillie <queen1892@gmail.com>
2021-02-28 19:11:38 -0600

    Adding Questions

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 // array of questions for user
 const questions = [ 
-    "How old are your?", 
-    "Do you prefer a truck or car?",
-    "Are you and early bird or night owl?" ];
+    {
+        type: "input",
+        message: "What is the titile of your project?",
+        name: "Title"
+    },
+    {
+        type:"input",
+        message: "Give a description of this Project",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "Give some instructions on installing this ReadMe",
+        name: "Installation Instruction"
+    },
+    {
+        type: "input",
+        message: "What is the purpose for using this ReadMe?",
+        name: "Usage Information"
+    },
+    {
+        type: "input",
+        message: "What are the guidelines contributions?",
+        name: "Contribution Guidelines"
+    },
+    {
+        type: "input",
+        message: "How are you testing the instructions in this file?",
+        name: "Test Instructions"
+    },
+    {
+        type: "list",
+        message: "You have to Select a License from One of the Following",
+        name: "Licenses",
+        choices: [
+            "Modified-BSD",
+            "Apache-2.0-only",
+            "CC0-1.0-only",
+            "Expat",
+            "GNU-GPL-3.0-or-later"
+        ]
+    },
+];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
